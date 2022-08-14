e33cf391247d12437ef9a78dcbf1f92f0a35f62c
Mykhailo Shyhin <johnglagol@gmail.com>
2022-08-14 14:35:50 +0300

    add hooks to modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,38 @@
 import OverlayStyled from './Overlay/Overlay.styled';
 import ModalStyled from './Modal.styled';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-    window.addEventListener('click', this.handleBackdropClick);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-    window.removeEventListener('click', this.handleBackdropClick);
-  }
-
-  handleKeyDown = e => {
+const Modal = ({ onClose, src }) => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return (
-      <OverlayStyled onClick={this.handleBackdropClick}>
-        <ModalStyled>
-          <img src={this.props.src} alt="" />
-        </ModalStyled>
-      </OverlayStyled>
-    );
-  }
-}
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('click', handleBackdropClick);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('click', handleBackdropClick);
+    };
+  }, []);
+
+  return (
+    <OverlayStyled onClick={handleBackdropClick}>
+      <ModalStyled>
+        <img src={src} alt="" />
+      </ModalStyled>
+    </OverlayStyled>
+  );
+};
 
 export default Modal;
 
